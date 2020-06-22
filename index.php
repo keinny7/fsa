e6d404976fcad80b80e17628778266c179498bd7
KurahaSho <joruresu4@gmailcom>
2020-06-22 18:24:19 -0500

    rutas

--- index.php
+++ index.php
@@ -10,16 +10,16 @@
 		<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
 
 		<!-- CSS here -->
-            <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-            <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-            <link rel="stylesheet" href="assets/css/slicknav.css">
-            <link rel="stylesheet" href="assets/css/animate.min.css">
-            <link rel="stylesheet" href="assets/css/magnific-popup.css">
-            <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
-            <link rel="stylesheet" href="assets/css/themify-icons.css">
-            <link rel="stylesheet" href="assets/css/slick.css">
-            <link rel="stylesheet" href="assets/css/nice-select.css">
-            <link rel="stylesheet" href="assets/css/style.css">
+            <link rel="stylesheet" href="view/assets/css/bootstrap.min.css">
+            <link rel="stylesheet" href="view/assets/css/owl.carousel.min.css">
+            <link rel="stylesheet" href="view/assets/css/slicknav.css">
+            <link rel="stylesheet" href="view/assets/css/animate.min.css">
+            <link rel="stylesheet" href="view/assets/css/magnific-popup.css">
+            <link rel="stylesheet" href="view/assets/css/fontawesome-all.min.css">
+            <link rel="stylesheet" href="view/assets/css/themify-icons.css">
+            <link rel="stylesheet" href="view/assets/css/slick.css">
+            <link rel="stylesheet" href="view/assets/css/nice-select.css">
+            <link rel="stylesheet" href="view/assets/css/style.css">
    </head> 
 
    <body class="body-bg">
@@ -29,7 +29,7 @@
             <div class="preloader-inner position-relative">
                 <div class="preloader-circle"></div>
                 <div class="preloader-img pere-text">
-                    <img src="assets/img/logo/logo.png" alt="">
+                    <img src="view/assets/img/logo/logo.png" alt="">
                 </div>
             </div>
         </div>
@@ -41,7 +41,7 @@
             <div class="header-wrapper d-flex flex-wrap align-items-center justify-content-between">
                 <!-- Logo -->
                 <div class="logo">
-                    <a href="?vista=main"><img src="assets/img/logo/logo.png" alt=""></a>
+                    <a href="?vista=main"><img src="view/assets/img/logo/logo.png" alt=""></a>
                 </div>
                 <!-- Main-menu -->
                 <div class="main-menu d-none d-lg-block">
@@ -168,38 +168,38 @@
 	<!-- JS here -->
 	
 		<!-- All JS Custom Plugins Link Here here -->
-        <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="./view/assets/js/vendor/modernizr-3.5.0.min.js"></script>
 		
 		<!-- Jquery, Popper, Bootstrap -->
-		<script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
-        <script src="./assets/js/popper.min.js"></script>
-        <script src="./assets/js/bootstrap.min.js"></script>
+		<script src="./view/assets/js/vendor/jquery-1.12.4.min.js"></script>
+        <script src="./view/assets/js/popper.min.js"></script>
+        <script src="./view/assets/js/bootstrap.min.js"></script>
 	    <!-- Jquery Mobile Menu -->
-        <script src="./assets/js/jquery.slicknav.min.js"></script>
+        <script src="./view/assets/js/jquery.slicknav.min.js"></script>
 
 		<!-- Jquery Slick , Owl-Carousel Plugins -->
-        <script src="./assets/js/owl.carousel.min.js"></script>
-        <script src="./assets/js/slick.min.js"></script>
+        <script src="./view/assets/js/owl.carousel.min.js"></script>
+        <script src="./view/assets/js/slick.min.js"></script>
 		<!-- One Page, Animated-HeadLin -->
-        <script src="./assets/js/wow.min.js"></script>
-		<script src="./assets/js/animated.headline.js"></script>
-        <script src="./assets/js/jquery.magnific-popup.js"></script>
+        <script src="./view/assets/js/wow.min.js"></script>
+		<script src="./view/assets/js/animated.headline.js"></script>
+        <script src="./view/assets/js/jquery.magnific-popup.js"></script>
 
 		<!-- Scrollup, nice-select, sticky -->
-        <script src="./assets/js/jquery.scrollUp.min.js"></script>
-        <script src="./assets/js/jquery.nice-select.min.js"></script>
-		<script src="./assets/js/jquery.sticky.js"></script>
+        <script src="./view/assets/js/jquery.scrollUp.min.js"></script>
+        <script src="./view/assets/js/jquery.nice-select.min.js"></script>
+		<script src="./view/assets/js/jquery.sticky.js"></script>
         
         <!-- contact js -->
-        <script src="./assets/js/contact.js"></script>
-        <script src="./assets/js/jquery.form.js"></script>
-        <script src="./assets/js/jquery.validate.min.js"></script>
-        <script src="./assets/js/mail-script.js"></script>
-        <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+        <script src="./view/assets/js/contact.js"></script>
+        <script src="./view/assets/js/jquery.form.js"></script>
+        <script src="./view/assets/js/jquery.validate.min.js"></script>
+        <script src="./view/assets/js/mail-script.js"></script>
+        <script src="./view/assets/js/jquery.ajaxchimp.min.js"></script>
         
 		<!-- Jquery Plugins, main Jquery -->	
-        <script src="./assets/js/plugins.js"></script>
-        <script src="./assets/js/main.js"></script>
+        <script src="./view/assets/js/plugins.js"></script>
+        <script src="./view/assets/js/main.js"></script>
         
     </body>
 </html>
