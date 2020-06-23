38bc2318e3c89fd002258c3621a656b31447532e
KurahaSho <joruresu4@gmailcom>
2020-06-22 19:18:02 -0500

    cambio estructural de carpetas

--- index.php
+++ index.php
@@ -94,8 +94,36 @@
         <!-- Header End -->
     </header>
     <main> 
-
+    
     <?php
+
+        if(isset($_POST['btnIniciar'])){
+
+
+        //valida el inicio de sesión
+        $validar = $usuario->validarU($_POST['txtUsuario'], $_POST['txtClave']);
+        if($validar == true){
+            $_SESSION['sesion_iniciada'] = true;
+            $datos = $usuario->datosUsuario($_POST['txtUsuario'], $_POST['txtClave']);
+            // nombre de usuario es nombre real
+            $_SESSION['nombreUsuario'] = $datos['NombreCompleto'];
+            // el usuario es el nickname
+            $_SESSION['usuario'] = $datos['Usuario'];
+            // el rol es el rol :V (si es administrativo, de comité o usuario común)
+            $_SESSION['rol'] = $datos['IdRol'];
+            $_SESSION['correo'] = $datos['Correo'];
+            include_once "vista/home.php";
+        }else{
+            $error = "<div class='alert alert-danger alert-dismissible' role='alert'>
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>×</span>
+            </button>
+            <strong>Error al iniciar sesión</strong> Usuario y/o contraseña incorrecta.
+            </div>";
+            include_once "vista/vlogin.php";
+            }
+        }
+
+        //retorna las páginas pedidas
         require_once "core/router.php";
         $router = new Router();
 
